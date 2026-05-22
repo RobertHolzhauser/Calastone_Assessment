ff2f336a96287336a34eb07ba046a21830bd39d6
Robert Holzhauser <RHolzhauser01@gmail.com>
2026-05-22 17:07:44 -0600

    progress ... instance issue in program.cs

---
+++ Calastone_TextFilter/Calastone_TextFilter/Models/InterogatedWord.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calastone_TextFilter.Models
+{
+    /***
+     * Interrogated word implies a word that HAS NOT YET been filtered, 
+     *   but has had any non-letters removed, for the sake of filtering.
+     *   while allowing the original word to be reconstructed with these characters post filtering.
+     *   Design note:  Any non letter chars could be contained in midChars, however, for simplicity I'm seperating out non word
+     *          characters into 3 categories of pre, post, and mid.  Pre and Post can be applied wholesale without regard to position.
+     *          Any midword characters will have to be reconstructed with regard to any position alterations potentially applied by filters.
+     **/ 
+    public class InterogatedWord
+    {
+        public string OriginalWord = string.Empty;  // this is the original non-modified word 
+        public string CleanedWord = string.Empty;  // this is the word after filtering.  Which may continue to be empty if a filter removes it. 
+        public string preFix = string.Empty;       // this will be used to re-apply post filtering any leading non-text chars
+        public string postFix = string.Empty;      // this will be used to re-apply post filtering any trailing non-text chars (punctuation, etc.)
+        public List<MidChars> midChars;            // this has any non AZaz chars in the middle of the word, and the position     
+    }
+}
---
+++ Calastone_TextFilter/Calastone_TextFilter/Models/MidChars.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+
+namespace Calastone_TextFilter.Models
+{
+    public class MidChars
+    {
+        public int midCharPosition = -1;
+        public char midChar;
+    }
+}
