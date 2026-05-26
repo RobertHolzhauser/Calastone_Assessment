124b4e34dab097ac41656a7419d09a15b46cbee8
Robert Holzhauser <RHolzhauser01@gmail.com>
2026-05-26 12:03:30 -0600

    Unit Test Base

---
+++ Calastone_TextFilter/TextFilter.Tests/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TextFilter.Factories;
+using TextFilter.Filters;
+using TextFilter.Interfaces;
+
+namespace TextFilter.Tests
+{
+    // https://github.com/pengweiqhca/Xunit.DependencyInjection/blob/main/README.md
+    public class StartUp
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddSingleton<Func<IEnumerable<ITextFilter>>>
+                    (x => () => x.GetService<IEnumerable<ITextFilter>>()!);
+            services.AddSingleton<IFilterFactory, FilterFactory>();
+            services.AddScoped<ITextFilter, Filter1>();
+            services.AddScoped<ITextFilter, Filter2>();
+            services.AddScoped<ITextFilter, Filter3>();
+        }
+    }
+}
--- Calastone_TextFilter/TextFilter.Tests/UnitTest1.cs
+++ Calastone_TextFilter/TextFilter.Tests/UnitTest1.cs
@@ -1,11 +1,50 @@
-﻿namespace TextFilter.Tests
+﻿using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using System.Security.Cryptography.X509Certificates;
+using TextFilter.Factories;
+using TextFilter.Filters;
+using TextFilter.Interfaces;
+using TextFilter.Models;
+using TextFilter.Services;
+using TextFilter.Interfaces;
+using TextFilter.Factories;
+using TextFilter.Filters;
+
+
+namespace TextFilter.Tests
 {
-    public class UnitTest1
+    
+
+    public class FilterTests
     {
+        private readonly IFilterFactory _filterFactory;
+        private readonly ITextFilter _filterSeq1;
+
+        public FilterTests(IFilterFactory filterFactory, ITextFilter filterSeq1)
+        {
+            _filterFactory = filterFactory;
+            _filterSeq1 = filterSeq1;
+        }
+
         [Fact]
         public void Test1()
         {
-
+            Assert.True(true);
         }
+
+        [Fact]
+        public void Filter3_Contains_t()
+        {
+            // Arrange 
+            InterrogatedWord word = new InterrogatedWord();
+            word.OriginalWord = "tank";
+
+            var filter3 = _filterFactory.Create(1);  // optimized sequence = filter 3 runs first  
+            var testedWord = filter3.Filter(word);
+  
+
+            Assert.Equal(testedWord.CleanedWord, "");
+        }
+
     }
 }
